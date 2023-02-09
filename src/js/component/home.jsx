c124c13f24b86b5ef77837e300a39a6f255c75d4
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-09 16:41:02 +0000

    finalizado navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,27 +6,32 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-	<nav className="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
-  <div className="container-fluid mx-5">
-  <span class="navbar-brand mb-0 h1">
-      Start Bootstrap
-    </span>
-	<ul className="nav justify-content-end">
-  <li className="nav-item">
-    <a className="nav-link text-white" aria-current="page" href="#">Home</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link text-white" href="#">About</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link text-white" href="#">Services</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link text-white" href="#">Contact</a>
-  </li>
-</ul>
-  </div>
-</nav>
+    <nav className="navbar navbar-expand-lg bg-body-secondary" data-bs-theme="dark">
+    <div className="container-fluid mx-3">
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
+      <div className="justify-content-end">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" ></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarColor01">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  </nav>
 	);
 };
 
