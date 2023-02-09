e8f2cdb389551bc63e24cfeb805fa4b95ac2977a
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-09 00:49:36 +0000

    terminado espacio de trabajo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,9 +6,27 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hola mundo</h1>
-		</div>
+	<nav className="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+  <div className="container-fluid mx-5">
+  <span class="navbar-brand mb-0 h1">
+      Start Bootstrap
+    </span>
+	<ul className="nav justify-content-end">
+  <li className="nav-item">
+    <a className="nav-link text-white" aria-current="page" href="#">Home</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link text-white" href="#">About</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link text-white" href="#">Services</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link text-white" href="#">Contact</a>
+  </li>
+</ul>
+  </div>
+</nav>
 	);
 };
 
