28b6a9fc9709a88acc2777ca1ce40b3e0d2a3f49
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-10 03:38:41 +0000

    finalizado card

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Nav from "./component/nav.jsx";
 import Jumbotron from "./component/jumbotron.jsx";
+import Card from './component/card.jsx';
 
 function App() {
   return (
     <div>
       <Nav />
-      <div>
+      <div className='container-fluid'>
       <Jumbotron />
+      <div className='row d-flex justify-content-around'>
+      <Card className="col-3"/> <Card className="col-3"/> <Card className="col-3"/> <Card className="col-3"/>
+      </div>
       </div>
     </div>
   );
---
+++ src/js/component/card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+//create your second component
+const Card = () => {
+	return (
+
+<div className="card px-0" style={{ width: "18rem" }}>
+  <img src="https://dummyimage.com/500x325/cccccc/f2f2f2.png" className="card-img-top" alt="..." />
+  <div className="card-body ">
+    <h5 className="card-title text-center">Card title</h5>
+    <p className="card-text text-center">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+  </div>
+  <ul className="list-group list-group-flush ">
+    <li className="list-group-item text-center"><a href="#" className="btn btn-primary">
+      Find Out More
+    </a></li>
+  </ul>
+</div>
+
+	);
+};
+
+export default Card;
